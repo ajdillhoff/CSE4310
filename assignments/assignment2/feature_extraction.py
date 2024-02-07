bbb91567d849ac5049694cd9ce41f292e6a22180
Alex Dillhoff <ajdillhoff@gmail.com>
2024-02-06 18:29:08 -0600

    Added assignment 2

---
+++ assignments/assignment2/feature_extraction.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# TODO: Create feature processing functions for SIFT and HOG
+
+if __name__ == "__main__":
+    # Load the pre-split data
+    data = np.load("cifar10.npz")
+
+    # TODO: Extract features from the training data
+
+    # TODO: Extract features from the testing data
+
+    # TODO: Save the extracted features to a file
